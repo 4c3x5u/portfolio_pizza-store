9dc9932205586514e9b8f4acf798f70b59017ac6
Alican Demirtas <demirtasdev@hotmail.com>
2020-12-23 16:29:18 +0000

    ChoosePizzaSize styles moved into own sass file.

--- frontend/src/Components/Order/Choose/PizzaSize/ChoosePizzaSize.jsx
+++ frontend/src/Components/Order/Choose/PizzaSize/ChoosePizzaSize.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Basket from '../Basket/Basket'
+import Basket from '../../Basket/Basket'
+import './ChoosePizzaSize.sass'
 
 const ChoosePizzaSize = () =>
   <section id="PizzaSize">
