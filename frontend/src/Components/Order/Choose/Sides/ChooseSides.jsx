13c159423c9dbe19188d77c6b5d1b10078ee19cb
Alican Demirtas <demirtasdev@hotmail.com>
2020-12-23 17:01:59 +0000

    ChooseSides styles moved into own sass file.

--- frontend/src/Components/Order/Choose/Sides/ChooseSides.jsx
+++ frontend/src/Components/Order/Choose/Sides/ChooseSides.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { Redirect } from 'react-router-dom'
 
-import { OrderContext } from '../Context/OrderStore'
-import { arrayEmpty } from '../util'
-import { getSides } from '../../../api'
+import { OrderContext } from '../../Context/OrderStore'
+import { arrayEmpty } from '../../util'
+import { getSides } from '../../../../api'
 
-import Basket from '../Basket/Basket'
+import Basket from '../../Basket/Basket'
 
 const ChooseSides = () => {
   const [availableSides, setAvailableSides] = useState([])
