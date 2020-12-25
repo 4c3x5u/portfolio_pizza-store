1aa22281b5269351066962fc8bd6461992af6c09
Alican Demirtas <demirtasdev@hotmail.com>
2020-12-25 03:43:15 +0000

    The 'active' class removed from all navLinks in the NotFound page.

--- frontend/src/Components/NotFound/NotFound.jsx
+++ frontend/src/Components/NotFound/NotFound.jsx
@@ -1,25 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { setNavLinkActive } from '../Order/util'
 
 import './NotFound.sass'
-import { Link } from 'react-router-dom'
 
-const NotFound = () =>
-  <section id="NotFound">
-    {console.log('NOT FOUND XXXXXXXXXXXXXXXX')}
-    <div id="PageContainer" className="container-fluid">
-      <div id="PageRow" className="row">
+const NotFound = () => {
+  useEffect(
+    () => { setNavLinkActive('None') },
+    []
+  )
+  return (
+    <section id="NotFound">
+      {console.log('NOT FOUND XXXXXXXXXXXXXXXX')}
+      <div id="PageContainer" className="container-fluid">
+        <div id="PageRow" className="row">
 
-        <article className="PageHead col-10 offset-1">
-          <h2 className="Header">NOT FOUND</h2>
-        </article>
+          <article className="PageHead col-10 offset-1">
+            <h2 className="Header">NOT FOUND</h2>
+          </article>
 
-        <article className="PageBody col-10 offset-1">
-          <h4 className="Message">Sorry... The page you&apos;re looking for doesn&apos;t exist.</h4>
-          <Link className="Back" to="/order">BACK</Link>
-        </article>
+          <article className="PageBody col-10 offset-1">
+            <h4 className="Message">Sorry... The page you&apos;re looking for doesn&apos;t exist.</h4>
+            <Link className="Back" to="/order">BACK</Link>
+          </article>
 
+        </div>
       </div>
-    </div>
-  </section>
-
+    </section>
+  )
+}
 export default NotFound
