3cb33f62ee15931d0637753f108d19c055446442
Alican Demirtas <demirtasdev@hotmail.com>
2020-12-30 08:00:14 +0000

    Test cases covered for empty pizzas, sides, drinks and orders.

--- frontend/src/Components/Order/util.test.js
+++ frontend/src/Components/Order/util.test.js
@@ -10,16 +10,14 @@ import {
 describe('arrayEmpty', () => {
   test('returns true for an empty array', () => {
     const emptyArray = [];
-    expect(
-      arrayEmpty(emptyArray),
-    ).toBe(true);
+    const result = arrayEmpty(emptyArray);
+    expect(result).toBe(true);
   });
 
   test('returns false for a non-empty array', () => {
     const nonEmptyArray = ['member1', 'member2'];
-    expect(
-      arrayEmpty(nonEmptyArray),
-    ).toBe(false);
+    const result = arrayEmpty(nonEmptyArray);
+    expect(result).toBe(false);
   });
 });
 
@@ -28,45 +26,60 @@ describe('orderEmpty', () => {
     const pizzas = [];
     const drinks = [];
     const sides = [];
-
-    expect(orderEmpty(pizzas, sides, drinks)).toBe(true);
+    const result = orderEmpty(pizzas, sides, drinks);
+    expect(result).toBe(true);
   });
 
   test('returns false if pizzas not empty', () => {
     const pizzas = ['pizza'];
     const drinks = [];
     const sides = [];
-
-    expect(orderEmpty(pizzas, sides, drinks)).toBe(false);
+    const result = orderEmpty(pizzas, sides, drinks);
+    expect(result).toBe(false);
   });
 
   test('returns false if drinks not empty', () => {
     const pizzas = [];
     const drinks = ['drink'];
     const sides = [];
-    expect(orderEmpty(pizzas, sides, drinks)).toBe(false);
+    const result = orderEmpty(pizzas, sides, drinks);
+    expect(result).toBe(false);
   });
 
   test('returns false if sides not empty', () => {
     const pizzas = [];
     const drinks = ['drink'];
     const sides = [];
-
-    expect(orderEmpty(pizzas, sides, drinks)).toBe(false);
+    const result = orderEmpty(pizzas, sides, drinks);
+    expect(result).toBe(false);
   });
 });
 
 describe('pizzasTotal', () => {
   test('returns the string representation of total price for pizzas', () => {
     const pizzas = [{ price: 12.99 }, { price: 14.49 }, { price: 12.24 }];
-    expect(pizzasTotal(pizzas)).toEqual('39.72');
+    const result = pizzasTotal(pizzas);
+    expect(result).toBe('39.72');
+  });
+
+  test('returns the string representation of 0 for empty pizzas', () => {
+    const pizzas = [];
+    const result = pizzasTotal(pizzas);
+    expect(result).toBe('0.00');
   });
 });
 
 describe('sidesOrDrinksTotal', () => {
   test('returns the string representation of total price for drinks or sides', () => {
-    const sides = [{ price: 2.99, quantity: 3 }, { price: 3.49, quantity: 5 }];
-    expect(sidesOrDrinksTotal(sides)).toEqual('26.42');
+    const sidesOrDrinks = [{ price: 2.99, quantity: 3 }, { price: 3.49, quantity: 5 }];
+    const result = sidesOrDrinksTotal(sidesOrDrinks);
+    expect(result).toBe('26.42');
+  });
+
+  test('returns the string representation of 0 for empty drinks or sides', () => {
+    const sidesOrDrinks = [];
+    const result = sidesOrDrinksTotal(sidesOrDrinks);
+    expect(result).toBe('0.00');
   });
 });
 
@@ -75,7 +88,15 @@ describe('orderTotal', () => {
     const pizzas = [{ price: 12.99 }, { price: 14.49 }, { price: 12.24 }];
     const sides = [{ price: 3.49, quantity: 1 }, { price: 2.99, quantity: 2 }];
     const drinks = [{ price: 1.20, quantity: 3 }, { price: 0.99, quantity: 1 }];
+    const result = orderTotal(pizzas, sides, drinks);
+    expect(result).toBe('53.78');
+  });
 
-    expect(orderTotal(pizzas, sides, drinks)).toBe('53.78');
+  test('returns the string representation of zero for empty orders', () => {
+    const pizzas = [];
+    const sides = [];
+    const drinks = [];
+    const result = orderTotal(pizzas, sides, drinks);
+    expect(result).toBe('0.00');
   });
 });
