d440a9f1aa415dfb2480a7f30c03a425e5af9949
Alican Demirtas <demirtasdev@hotmail.com>
2020-12-30 07:36:46 +0000

    Tests added for the orderTotal function.

--- frontend/src/Components/Order/util.test.js
+++ frontend/src/Components/Order/util.test.js
@@ -3,6 +3,7 @@ import {
   arrayEmpty,
   orderEmpty,
   pizzasTotal,
+  orderTotal,
 } from './util';
 
 describe('arrayEmpty', () => {
@@ -31,40 +32,43 @@ describe('orderEmpty', () => {
     expect(orderEmpty(order.pizzas, order.sides, order.drinks)).toBe(true);
   });
 
-  test('returns false for an order that has non-empty pizzas', () => {
-    const order = {
-      memberId: '',
-      pizzas: ['pizza'],
-      drinks: [],
-      sides: [],
-    };
-    expect(orderEmpty(order.pizzas, order.sides, order.drinks)).toBe(false);
+  test('returns false if pizzas not empty', () => {
+    const pizzas = ['pizza'];
+    const drinks = [];
+    const sides = [];
+
+    expect(orderEmpty(pizzas, sides, drinks)).toBe(false);
   });
 
-  test('returns false for an order that has non-empty drinks', () => {
-    const order = {
-      pizzas: [],
-      drinks: ['drink'],
-      sides: [],
-    };
-    expect(orderEmpty(order.pizzas, order.sides, order.drinks)).toBe(false);
+  test('returns false if drinks not empty', () => {
+    const pizzas = [];
+    const drinks = ['drink'];
+    const sides = [];
+    expect(orderEmpty(pizzas, sides, drinks)).toBe(false);
   });
 
-  test('returns false for an order that has non-empty sides', () => {
-    const order = {
-      pizzas: [],
-      drinks: ['drink'],
-      sides: [],
-    };
-    expect(orderEmpty(order.pizzas, order.sides, order.drinks)).toBe(false);
+  test('returns false if sides not empty', () => {
+    const pizzas = [];
+    const drinks = ['drink'];
+    const sides = [];
+
+    expect(orderEmpty(pizzas, sides, drinks)).toBe(false);
   });
 });
 
 describe('pizzasTotal', () => {
   test('returns the string representation of total price for pizzas', () => {
-    const pizzas = [
-      { price: 12.99 }, { price: 14.49 }, { price: 12.24 },
-    ];
+    const pizzas = [{ price: 12.99 }, { price: 14.49 }, { price: 12.24 }];
     expect(pizzasTotal(pizzas)).toEqual('39.72');
+  });
+});
+
+describe('orderTotal', () => {
+  test('returns the string representation of the subtotal for all order item', () => {
+    const pizzas = [{ price: 12.99 }, { price: 14.49 }, { price: 12.24 }];
+    const sides = [{ price: 3.49, quantity: 1 }, { price: 2.99, quantity: 2 }];
+    const drinks = [{ price: 1.20, quantity: 3 }, { price: 0.99, quantity: 1 }];
+
+    expect(orderTotal(pizzas, sides, drinks)).toBe('27.48');
   });
 });
