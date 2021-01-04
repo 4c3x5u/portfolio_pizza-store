471c54ac6ac1e710995bd4444190c99a7f8b6a98
Alican Demirtas <demirtasdev@hotmail.com>
2021-01-04 18:23:56 +0000

    Tests brought up to a working state again.

--- frontend/src/__mocks__/server/handlers/orderHandlers.js
+++ frontend/src/__mocks__/server/handlers/orderHandlers.js
@@ -1,22 +1,23 @@
 import { rest } from 'msw';
-import orderHistory from '../../data/order-history.json';
 
 const orderHandlers = [
-  rest.get('http://localhost:4000/order/history/:memberId', (req, res, ctx) => (
-    res(ctx.status(200), ctx.json(
-      orderHistory.find((oh) => oh.member === req.params.memberId).orders,
-    ))
-  )),
+  rest.get('http://localhost:4000/order/history/:memberId', (req, res, ctx) => {
+    if (req.body === {} || req.params.memberId !== '5fe8a190177d4f52b7d8bfb2') {
+      return res(ctx.status(400), ctx.json({ errorMessage: 'Invalid order history request.' }));
+    }
+    return res(ctx.status(200), ctx.text('Order history request successful.'));
+  }),
 
   rest.post('http://localhost:4000/order', (req, res, ctx) => {
     if (req.body === undefined || req.body.address === undefined
         || req.body.date === undefined || req.body.memberId === undefined
         || req.body.paymentDetails === undefined || req.body.phoneNumber === undefined) {
-      return res(ctx.status(400));
+      return res(ctx.status(400), ctx.json({
+        errorMessage: 'One or more order submission fields undefined.',
+      }));
     }
     return res(ctx.status(200), ctx.json(req.body));
   }),
 ];
 
 export default orderHandlers;
-
--- frontend/src/__tests__/api.test.js
+++ frontend/src/__tests__/api.test.js
@@ -80,7 +80,7 @@ describe('validateAuthTokens', () => {
       .then((result) => expect(result).toStrictEqual({
         error: {
           status: 400,
-          message: 'Token validation failed.',
+          message: 'One or more token validation request fields undefined.',
         },
       })).catch((error) => expect(error).toBeUndefined());
   });
@@ -171,7 +171,12 @@ describe('submitOrder', () => {
   it('should return an error message on empty submission', () => {
     expect.hasAssertions();
     return submitOrder()
-      .then((result) => expect(result).toStrictEqual({ message: 'Failed to submit the order.' }))
+      .then((result) => expect(result).toStrictEqual({
+        error: {
+          status: 400,
+          message: 'One or more order submission fields undefined.',
+        },
+      }))
       .catch((error) => expect(error).toBeUndefined());
   });
 });
@@ -179,71 +184,20 @@ describe('submitOrder', () => {
 describe('getOrderHistory', () => {
   it('should get the order history for a given valid memberId', () => {
     expect.hasAssertions();
-    const orders = [{
-      _id: { $oid: '5fe643700191b8b234d95c9e' },
-      __v: 0,
-      address: {
-        firstLine: '31 Here Street',
-        secondLine: 'Over-There Rd.',
-        postcode: 'EN2 0DE',
-      },
-      date: 'December 25th 2020, 7:54:06 pm',
-      drinks: [
-        {
-          _id: { $oid: '5fe643700191b8b234d95ca0' },
-          name: 'Coca-Cola',
-          quantity: 1,
-        },
-        {
-          _id: { $oid: '5fe643700191b8b234d95ca1' },
-          name: 'Ice-Tea Lemon',
-          quantity: 3,
-        },
-      ],
-      memberId: '4fa54264d372a605a82a200d',
-      paymentDetails: {
-        cardNumber: '4111 1111 1111 1111',
-        expiryDate: '12/42',
-        securityCode: '123',
-      },
-      phoneNumber: '09876543210',
-      pizzas: [
-        {
-          toppings: ['Onion', 'Extra Cheese'],
-          _id: { $oid: '5fe643700191b8b234d95c9f' },
-          size: 'medium',
-          price: 14.65,
-        },
-      ],
-      sides: [
-        {
-          _id: { $oid: '5fe643700191b8b234d95ca2' },
-          name: 'Chicken Wings',
-          quantity: 4,
-        },
-        {
-          _id: { $oid: '5fe643700191b8b234d95ca3' },
-          name: 'Potato Chips',
-          quantity: 2,
-        },
-      ],
-    }];
     return getOrderHistory('5fe8a190177d4f52b7d8bfb2')
-      .then((result) => expect(result).toStrictEqual(orders))
+      .then((result) => expect(result).toStrictEqual('Order history request successful.'))
       .catch((error) => expect(error).toBeUndefined());
   });
 
-  it('should return an error message for a given invalid memberId', () => {
-    expect.hasAssertions();
-    return getOrderHistory('asdklfjasldfkjasldfkjaka')
-      .then((result) => expect(result).toStrictEqual({ message: 'Failed to get order history.' }))
-      .catch((error) => expect(error).toBeUndefined());
-  });
-
-  it('should return an error message if a memberId is not supplied', () => {
+  it('returns an error message for an invalid order history request', () => {
     expect.hasAssertions();
     return getOrderHistory()
-      .then((result) => expect(result).toStrictEqual({ message: 'Failed to get order history.' }))
+      .then((result) => expect(result).toStrictEqual({
+        error: {
+          status: 400,
+          message: 'Invalid order history request.',
+        },
+      }))
       .catch((error) => expect(error).toBeUndefined());
   });
 });
