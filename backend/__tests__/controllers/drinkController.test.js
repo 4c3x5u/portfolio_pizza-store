ca7a2cd341bbd4095dff4f30915bc9bef5902014
Alican Demirtas <demirtasdev@hotmail.com>
2021-01-08 06:26:49 +0000

    mockgoose has been removed from the project

--- backend/__tests__/controllers/drinkController.test.js
+++ backend/__tests__/controllers/drinkController.test.js
@@ -1,38 +1,25 @@
 import mongoose from 'mongoose';
-import { Mockgoose } from 'mockgoose';
 import request from 'supertest';
 
 import app from '../../app';
 import { connectionString } from '../../.secrets/mongo.json';
 import drinks from '../data/drinks.json';
 
-const mockgoose = new Mockgoose(mongoose);
-
 describe('getDrinks', () => {
   beforeAll(() => {
-    mockgoose.prepareStorage().then(() => {
-      mongoose.connect(connectionString);
-    });
+    mongoose.connect(connectionString);
   });
 
   afterAll(() => {
     mongoose.connection.close();
   });
 
-  it('should return the complete list of all drinks in the database', (done) => (
+  it('should return the complete list of all drinks in the database', () => (
     request(app)
       .get('/drinks')
       .then((response) => {
         expect(response.status).toBe(200);
-        // console.log('response.body:', response.body);
-        // console.log('drinks:', drinks);
         expect(response.body).toStrictEqual(drinks);
-        // console.log(response.body);
-        done();
-      })
-      .catch((error) => {
-        console.error(error);
-        done();
       })
   ));
 });
