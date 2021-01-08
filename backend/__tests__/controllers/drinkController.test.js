2384543977e6aece1e7959e9c211e14377aeea01
Alican Demirtas <demirtasdev@hotmail.com>
2021-01-08 06:39:36 +0000

    Successfully mocked MongoDB using Mockingoose, and have written the first successful unit test for the backend.

--- backend/__tests__/controllers/drinkController.test.js
+++ backend/__tests__/controllers/drinkController.test.js
@@ -1,25 +1,24 @@
-import mongoose from 'mongoose';
 import request from 'supertest';
+import mongoose from 'mongoose';
+import mockingoose from 'mockingoose';
 
 import app from '../../app';
-import { connectionString } from '../../.secrets/mongo.json';
 import drinks from '../data/drinks.json';
+import DrinkSchema from '../../models/drinkModel';
+
+const Drink = mongoose.model('Drink', DrinkSchema);
 
 describe('getDrinks', () => {
   beforeAll(() => {
-    mongoose.connect(connectionString);
+    mockingoose(Drink).toReturn(drinks, 'find');
   });
 
-  afterAll(() => {
-    mongoose.connection.close();
-  });
-
-  it('should return the complete list of all drinks in the database', () => (
-    request(app)
+  it('should return the complete list of all drinks in the database', () => {
+    return request(app)
       .get('/drinks')
       .then((response) => {
         expect(response.status).toBe(200);
         expect(response.body).toStrictEqual(drinks);
-      })
-  ));
+      });
+  });
 });
