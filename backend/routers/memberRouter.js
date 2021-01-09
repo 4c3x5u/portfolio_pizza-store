f8530910964dac75874d9b609f13ac27d1579659
Alican Demirtas <demirtasdev@hotmail.com>
2021-01-09 08:54:45 +0000

    Validation blocks for the member router moved into their own file for better readability.

--- backend/routers/memberRouter.js
+++ backend/routers/memberRouter.js
@@ -1,49 +1,24 @@
 import express from 'express';
-import { body } from 'express-validator';
 
 import { register, signIn, validateToken } from '../controllers/memberController';
+import validateMember from './validation/validateMember';
 
 const memberRouter = express.Router();
 
 memberRouter.post('/register', [
-  body('email')
-    .isEmail()
-    .withMessage('Invalid email.'),
-  body('password')
-    .isAscii()
-    .isLength({ min: 8, max: 35 })
-    .withMessage('Invalid password.'),
-  body('passwordConfirmation')
-    .isAscii()
-    .isLength({ min: 8, max: 35 })
-    .custom((value, { req }) => {
-      if (value !== req.body.password) {
-        throw new Error('Password confirmation does not match password.');
-      }
-      return true;
-    })
-    .withMessage('Invalid password confirmation.'),
+  validateMember.email(),
+  validateMember.password(),
+  validateMember.passwordConfirmation(),
 ], register);
 
 memberRouter.post('/sign-in', [
-  body('email')
-    .isEmail()
-    .withMessage('Invalid email.'),
-  body('password')
-    .isLength({ min: 8, max: 35 })
-    .isAscii()
-    .withMessage('Invalid password.'),
+  validateMember.email(),
+  validateMember.password(),
 ], signIn);
 
 memberRouter.post('/validate-token', [
-  body('user')
-    .not().isEmpty()
-    .isHexadecimal()
-    .withMessage('Invalid user.'),
-  body('token')
-    .not().isEmpty()
-    .isAscii()
-    .withMessage('Invalid token'),
+  validateMember.user(),
+  validateMember.token(),
 ], validateToken);
 
 export default memberRouter;
